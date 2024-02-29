1bc636ecdd13cfbe51a3faf627908cca297c404e
VedantGadge <vedant.gadgegsis@gmail.com>
2024-02-29 21:56:04 +0530

    On branch main Changes to be committed: new file:   Exp7_a.class modified:   Exp7_a.java deleted:    README.md

--- Exp7_a.java
+++ Exp7_a.java
@@ -1,12 +1,15 @@
 public class Exp7_a 
 {
-    int count=0;
+    static int count=0;
     Exp7_a()
     {
         count++;
     }
     public static void main(String[] args) 
     {
-        
+        Exp7_a ob1 = new Exp7_a();
+        Exp7_a ob2 = new Exp7_a();
+        Exp7_a ob3 = new Exp7_a();
+        System.out.println("The number of objects: "+count);
     }
 }
